046ddd16b40ad477abc2c725206de6826d00d9c3
ethelfernando <86898161+ethelfernando@users.noreply.github.com>
2022-03-25 13:24:10 +1100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,5 @@
-module "network" {
+module "network" 
+  {
 source = "app.terraform.io/dhivi0815/network/azurerm"
 version = "3.5.0"
 resource_group_name = azurerm_resource_group.myresourcegroup.name
